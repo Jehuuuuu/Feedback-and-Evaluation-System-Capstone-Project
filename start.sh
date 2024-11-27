17f4d314eba4e950f8f43253e240b249449eb146
Jehu Paulo C. Rodriguez <jehurodriguez143@gmail.com>
2024-11-27 09:21:11 +0800

    commit changes before production

--- start.sh
+++ start.sh
@@ -3,6 +3,9 @@
 set -o errexit
 
 pip install -r requirements.txt
-
+python nltk_downloader.py
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Start your application
+python manage.py runserver 0.0.0.0:8000
