15ffc3b384fe3e55626a5e54e9e57209fd6caa03
Jehu Paulo C. Rodriguez <jehurodriguez143@gmail.com>
2024-11-27 09:52:32 +0800

    commit changes before production

--- Rodriguez/settings.py
+++ Rodriguez/settings.py
@@ -31,7 +31,12 @@ SECRET_KEY = 'django-insecure-pgt85)y^t95n#bdtt(pr24&l+2d(1t$pd-3g*aj%vo$swhq=hm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    'feedback-and-evaluation-system-capstone.onrender.com'
+]
+
 
 
 # Application definition
