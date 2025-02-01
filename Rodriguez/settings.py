d70984bf5795c52b3f037a275b9df28b85950801
Jehu Paulo C. Rodriguez <jehurodriguez143@gmail.com>
2025-02-01 20:40:50 +0800

    Refactor settings configuration: streamline static and media file paths, and update static file storage settings for improved organization

--- Rodriguez/settings.py
+++ Rodriguez/settings.py
@@ -127,21 +127,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-# Static files (CSS, JavaScript, Images)
-STATIC_URL = '/static/'  # URL to access static files in development or production
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'Feedbacksystem', 'static'),  # Path for additional static files in development
-]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Path where static files will be collected for production
-
-# Static file storage for production
+STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+MEDIA_ROOT = '/media/'
 
-# Media files (Uploaded files)
-MEDIA_URL = '/media/'  # URL to access media files in development or production
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # Path to store uploaded files
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+STATICFILES_DIR = [
+    os.path.join(BASE_DIR, 'static')
+]
 
+MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_URL = "/media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
